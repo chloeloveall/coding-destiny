5460e39ef6f34d08eef2708cfb6e2ed46a3c90ff
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-15 16:34:35 -0800

    Add styling to body and h elements, add form and it's divs to index.html, attempt to write js for form input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  
+  $('#form-destiny').submit(function(event) {
+    const favoriteColorInput = $('input#favoriteColor').val();
+
+    $('.favoriteColor').text(favoriteColorInput);
+
+    event.preventDefault();
+  })
 }
