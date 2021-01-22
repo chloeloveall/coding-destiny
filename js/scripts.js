e0572de5b3ea6be97460872648fe6e69c9a28e95
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-21 20:42:33 -0800

    Add a clunky way to hide and show language divs, and reset form on submit with jQuery reset

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 $(document).ready(function() {
   $('#form-destiny').submit(function(event) {
     event.preventDefault();
+
     const favoriteColorInput = $('input#favoriteColor').val();
     const userAgeInput = parseInt($('input#userAge').val());
 
     if (favoriteColorInput === 'green' && userAgeInput < 35) {
       $('.learn-js').show();
+      $('.learn-ruby').hide();
+      $('.learn-python').hide();
     } else if (favoriteColorInput === 'purple' && userAgeInput > 20) {
       $('.learn-ruby').show();
+      $('.learn-js').hide();
+      $('.learn-python').hide();
     } else {
       $('.learn-python').show();
+      $('.learn-js').hide();
+      $('.learn-ruby').hide();
     }
-
+    $('#form-destiny')[0].reset();
   });
+
+  // $('button.resetBtn').submit(function(event) {
+  //   $('.learn-js').hide();
+  // })
+
 });
