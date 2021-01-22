a75ea747a98ffc08bd7c5cbaa954ae173ec21394
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-21 18:28:13 -0800

    Add if/else statment, use jQuery show method, use pareseInt for age input

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
   $('#form-destiny').submit(function(event) {
     event.preventDefault();
     const favoriteColorInput = $('input#favoriteColor').val();
+    const userAgeInput = parseInt($('input#userAge').val());
 
-    $('.favoriteColor').text(favoriteColorInput);
+    if (favoriteColorInput === 'green' && userAgeInput < 35) {
+      $('.learn-js').show();
+    } else if (favoriteColorInput === 'purple' && userAgeInput > 20) {
+      $('.learn-ruby').show();
+    } else {
+      $('.learn-python').show();
+    }
 
   });
 });
