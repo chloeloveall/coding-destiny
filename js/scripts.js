874eecf07ca38875b9ebe566b0824b73155f3b3c
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-21 20:57:03 -0800

    Define a variable to accept user input for mood, add mood to if/else, change mood question on index.html

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,9 @@ $(document).ready(function() {
 
     const favoriteColorInput = $('input#favoriteColor').val();
     const userAgeInput = parseInt($('input#userAge').val());
+    const moodInput = $('input#mood').val();
 
-    if (favoriteColorInput === 'green' && userAgeInput < 35) {
+    if (favoriteColorInput === 'green' && userAgeInput < 35 || moodInput === 'yes') {
       $('.learn-js').show();
       $('.learn-ruby').hide();
       $('.learn-python').hide();
