c67889d25904574d314909938b9a7e519ffa21e4
Chloe Loveall <5696284+chloeloveall@users.noreply.github.com>
2021-01-21 21:57:28 -0800

    Define const for user creativity input and incorporate into if/else

--- js/scripts.js
+++ js/scripts.js
@@ -5,12 +5,13 @@ $(document).ready(function() {
     const favoriteColorInput = $('input#favoriteColor').val().toLowerCase();
     const userAgeInput = parseInt($('input#userAge').val());
     const moodInput = $('input#mood').val().toLowerCase();
+    const creativityInput = $('input#creativity').val().toLowerCase();
 
     if (favoriteColorInput === 'green' && userAgeInput < 35 || moodInput === 'yes') {
       $('.learn-js').show();
       $('.learn-ruby').hide();
       $('.learn-python').hide();
-    } else if (favoriteColorInput === 'purple' && userAgeInput > 20) {
+    } else if (favoriteColorInput === 'purple' && userAgeInput > 20 || creativityInput === 'no') {
       $('.learn-ruby').show();
       $('.learn-js').hide();
       $('.learn-python').hide();
